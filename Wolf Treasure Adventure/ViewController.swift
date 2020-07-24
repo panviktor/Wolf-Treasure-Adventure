6bbed8cd71eaf6c9b594a3623b1b56ed14a73e1b
SwiftCoder <SwiftCoder@nossvwpdf.ls>
2020-07-24 15:19:28 +0300

    Initial Commit

---
+++ Wolf Treasure Adventure/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Wolf Treasure Adventure
+//
+//  Created by Viktor on 24.07.2020.
+//  Copyright © 2020 Viktor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
